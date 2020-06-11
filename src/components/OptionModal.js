eaabcee1baa40392696f5aeb3e31e009ede90d8c
krishan-kant <krish.kant.47@gmail.com>
2020-06-11 22:32:01 +0530

    styled with scss

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -7,10 +7,12 @@ const OptionModal = (props) => (
         isOpen={!!props.selectedOption}
         onRequestClose={props.handleBack} 
         contentLabel="SelectedOption"
+        closeTimeoutMS={400}
+        className="modal"
     >
-        <h3>Selected Option</h3>
-        {props.selectedOption && <p>{props.selectedOption}</p>}
-        <button onClick={props.handleBack}>Okay!</button>
+        <h3 className="modal__title">Selected Option</h3>
+        {props.selectedOption && <p className="modal__body">{props.selectedOption}</p>}
+        <button className="button" onClick={props.handleBack}>Okay!</button>
     </Modal>
 );
 
--- src/components/Options.js
+++ src/components/Options.js
@@ -4,11 +4,14 @@ import Option from './Option';
 const Options = (props) => {
     return (
         <div>
-            <button onClick={props.handleDeleteOptions}>Remove all</button>
-            {props.options.length === 0 && <p>Add an item to get started!</p>}
+            <div className="widget-header">
+                <h3 className="widget-header__title">Your Options</h3>
+                <button onClick={props.handleDeleteOptions} className="button button--link">Remove all</button>
+            </div>
+            {props.options.length === 0 && <p className="widget__message">Add an item to get started!</p>}
             {
-                props.options.map((option) => {
-                    return <Option key={option} optionText={option} handleDeleteOption={props.handleDeleteOption}/>
+                props.options.map((option,index) => {
+                    return <Option key={option} count={index+1} optionText={option} handleDeleteOption={props.handleDeleteOption}/>
                 })
             }
         </div>
